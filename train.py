61ad521ff08b9f276224d447799ce6099f82ca29
Ben Liggett <bliggett@macbook-pro.mynetworksettings.com>
2022-09-29 09:58:19 -0400

    adjusted args for saving model

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ Use:
 import argparse
 
 from zoo.utils.parse_yaml import parse_yaml
-from zoo.models.init_model import init_model
+from zoo.utils.init_training import init_training
 
 parser = argparse.ArgumentParser()
 required = parser.add_argument_group("required arguments")
@@ -16,11 +16,13 @@ required.add_argument("--epochs", type=int, required=True, help="Enter training 
 required.add_argument("--data", type=str, required=True, help="Enter yaml path location for dataset")
 optional.add_argument("--batch-size", type=int, default=16, help="total bactchsize for gpus")
 optional.add_argument("--force-data", action="store_true", help="force generate new data")
+optional.add_argument("--dynamic", action="store_true", help="force program to generate samples during runtime")
 
 def run(args):
     args.data = parse_yaml(args.data)
-    init_model(vars(args))
-
+    obj_detector = init_training(vars(args))
+    obj_detector.param = args
+    obj_detector.train()
 
 if __name__ == "__main__":
     args = parser.parse_args()
