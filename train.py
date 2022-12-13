11073c9d5448860c8c525eda62e574cc9d9d8853
Ben Liggett <bliggett@bens-mbp.mynetworksettings.com>
2022-12-13 09:06:42 -0500

    adjusted train func

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from zoo.utils.init_training import init_training
 parser = argparse.ArgumentParser()
 required = parser.add_argument_group("required arguments")
 optional = parser.add_argument_group("optional arguments")
-required.add_argument("--model", choices=["rcnn"], required=True, help="Select model for training")
+required.add_argument("--model", choices=["rcnn", "fasterrcnn"], required=True, help="Select model for training")
 required.add_argument("--epochs", type=int, required=True, help="Enter training epochs")
 required.add_argument("--data", type=str, required=True, help="Enter yaml path location for dataset")
 optional.add_argument("--batch-size", type=int, default=16, help="total bactchsize for gpus")
